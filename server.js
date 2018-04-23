fa77a5a121691110c38495a0d1177f9d9a101a35
wahidc7101 <wahidc7101@gmail.com>
2018-04-23 16:42:23 -0400

    submit assigment

--- server.js
+++ server.js
@@ -50,7 +50,29 @@ app.post('/shopping-list', jsonParser, (req, res) => {
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
-})
+});
+
+// My code
+app.post('/recipes', jsonParser, (req, res) 
+  => {
+const requiredFields = ['name', 'ingredients'];
+
+for (let i=0; i<requiredFields.length; i++) {
+  const field = requiredFields[i];
+  if (!(field in req.body)) {
+    const message `Missing \`${field}\` in request
+    body`
+    console.error(message);
+    return res.status(400).send(message);
+  }
+}
+
+const item = Recipes.create(req.body.name, req.body.budget);
+res.status(201).json(item);
+
+});
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
